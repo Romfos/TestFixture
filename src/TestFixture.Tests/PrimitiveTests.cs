c9d4514780e2d8f92f0b23d7ba83ed012e92e93a
Roman Davydovich <Roman.Davydovich@kaseya.com>
2022-09-30 18:56:20 +0200

    Added basic infrastructure

---
+++ src/TestFixture.Tests/PrimitiveTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestFixture.Tests;
+
+[TestClass]
+public sealed class PrimitiveTests
+{
+    private readonly Fixture fixture = new Fixture();
+
+    [TestMethod]
+    public void Int32()
+    {
+        fixture.Create<int>();
+    }
+
+    [TestMethod]
+    public void Guid()
+    {
+        fixture.Create<Guid>();
+    }
+}
---
+++ src/TestFixture/Fixture.cs
@@ -0,0 +1,37 @@
+using Autofac;
+using System;
+
+namespace TestFixture
+{
+    public sealed class Fixture
+    {
+        private readonly IContainer container;
+
+        public Fixture()
+        {
+            var containerBuilder = new ContainerBuilder();
+            Register(containerBuilder);
+            container = containerBuilder.Build();
+        }
+
+        public Fixture(Action<ContainerBuilder> builder)
+        {
+            var containerBuilder = new ContainerBuilder();
+            Register(containerBuilder);
+            builder(containerBuilder);
+            container = containerBuilder.Build();
+        }
+
+        private void Register(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterInstance(new Random()).AsSelf().SingleInstance();
+            containerBuilder.Register(x => x.Resolve<Random>().Next()).AsSelf().InstancePerDependency();
+            containerBuilder.Register(x => Guid.NewGuid()).AsSelf().InstancePerDependency();
+        }
+
+        public T Create<T>()
+        {
+            return container.Resolve<T>();
+        }
+    }
+}
