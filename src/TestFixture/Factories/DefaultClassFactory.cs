7265b48afaef84bbfce72d07546679cb2e7fde6f
Romfos <Romfos@tut.by>
2022-10-09 21:31:30 +0000

    Hotfix DefaultClassFactory

--- src/TestFixture/Factories/DefaultClassFactory.cs
+++ src/TestFixture/Factories/DefaultClassFactory.cs
@@ -17,20 +17,21 @@ internal sealed class DefaultClassFactory : IFactory
 
     public object Create(Fixture fixture)
     {
-        var target = constructorInfo.Invoke(ResolveParameters(););
-        ResolveProperties(target);
-        ResolveFields(target);
+        var arguments = ResolveParameters(fixture);
+        var target = constructorInfo.Invoke(arguments);
+        ResolveProperties(fixture, target);
+        ResolveFields(fixture, target);
         return target;
     }
     
-    private object[] ResolveParameters()
+    private object[] ResolveParameters(Fixture fixture)
     {
         return constructorInfo.GetParameters()
             .Select(x => fixture.Create(x.ParameterType))
             .ToArray();
     }
     
-    private void ResolveProperties(object target)
+    private void ResolveProperties(Fixture fixture, object target)
     {
         foreach (var property in type.GetProperties().Where(x => x.CanWrite))
         {
@@ -38,7 +39,7 @@ internal sealed class DefaultClassFactory : IFactory
         }
     }
     
-    private void ResolveFields(object target)
+    private void ResolveFields(Fixture fixture, object target)
     {
         foreach (var field in type.GetFields().Where(x => x.IsPublic))
         {
