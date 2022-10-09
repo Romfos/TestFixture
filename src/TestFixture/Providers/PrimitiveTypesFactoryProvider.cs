0f2b87a2fbdc8c9cbc8a41c9cf47b7a6cc05e143
Romfos <Romfos@tut.by>
2022-10-09 15:32:44 +0000

    Update PrimitiveTypesFactoryProvider

--- src/TestFixture/Providers/PrimitiveTypesFactoryProvider.cs
+++ src/TestFixture/Providers/PrimitiveTypesFactoryProvider.cs
@@ -5,23 +5,19 @@ namespace TestFixture.Factories;
 
 internal sealed class PrimitiveTypesFactoryProvider : IFactoryProvider
 {
-    private readonly Int32Factory int32Factory = new();
-    private readonly GuidFactory guidFactory = new();
-    private readonly StringFactory stringFactory = new();
-
     public IFactory? Resolve(Type type)
     {
         if (type == typeof(int))
         {
-            return int32Factory;
+            return new Int32Factory();
         }
         if (type == typeof(Guid))
         {
-            return guidFactory;
+            return new GuidFactory();
         }
         if (type == typeof(string))
         {
-            return stringFactory;
+            return new StringFactory();
         }
         return null;
     }
