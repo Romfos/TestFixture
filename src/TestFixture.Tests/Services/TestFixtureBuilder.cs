c2338c1527b8bbf93abbc8b4ffea00ccdafbc07a
Romfos <Romfos@tut.by>
2024-12-25 22:45:17 +0100

    Adopt params IEnumerable

--- src/TestFixture.Tests/Services/TestFixtureBuilder.cs
+++ src/TestFixture.Tests/Services/TestFixtureBuilder.cs
@@ -7,42 +7,42 @@ internal sealed class TestFixtureBuilder
 {
     private readonly IRandomService substitute = Substitute.For<IRandomService>();
 
-    public TestFixtureBuilder With(params int[] values)
+    public TestFixtureBuilder With(params IEnumerable<int> values)
     {
         var queue = new Queue<int>(values);
         substitute.Int32.Returns(x => queue.Dequeue());
         return this;
     }
 
-    public TestFixtureBuilder With(params double[] values)
+    public TestFixtureBuilder With(params IEnumerable<double> values)
     {
         var queue = new Queue<double>(values);
         substitute.Double.Returns(x => queue.Dequeue());
         return this;
     }
 
-    public TestFixtureBuilder With(params Guid[] values)
+    public TestFixtureBuilder With(params IEnumerable<Guid> values)
     {
         var queue = new Queue<Guid>(values);
         substitute.Guid.Returns(x => queue.Dequeue());
         return this;
     }
 
-    public TestFixtureBuilder With(params string[] values)
+    public TestFixtureBuilder With(params IEnumerable<string> values)
     {
         var queue = new Queue<string>(values);
         substitute.String.Returns(x => queue.Dequeue());
         return this;
     }
 
-    public TestFixtureBuilder With(params long[] values)
+    public TestFixtureBuilder With(params IEnumerable<long> values)
     {
         var queue = new Queue<long>(values);
         substitute.Int64.Returns(x => queue.Dequeue());
         return this;
     }
 
-    public TestFixtureBuilder With(params DateTime[] values)
+    public TestFixtureBuilder With(params IEnumerable<DateTime> values)
     {
         var queue = new Queue<DateTime>(values);
         substitute.DateTime.Returns(x => queue.Dequeue());
